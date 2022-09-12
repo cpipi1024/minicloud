9521fa2995428b047f6defa7c0185863f69441ee
yangtao <1461481767@qq.com>
2022-09-13 01:44:50 +0800

    🚧 自定义gin中间件

--- middlewares/default.go
+++ middlewares/default.go
@@ -1,17 +1,90 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"runtime/debug"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
 
 // todo: 自定义日志中间件
-func GinLogger() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+func GinLogger(logger *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		method := c.Request.Method
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		c.Next()
+		cost := time.Since(start)
 
+		// 使用zap进行打印
+		logger.Info(
+			path,
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", method),
+			zap.String("path", path),
+			zap.String("query", query),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+			zap.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
+			zap.Duration("cost", cost),
+		)
 	}
 }
 
 // todo: 自定义panic recover
-func GinRecover() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+func GinRecover(logger *zap.Logger, stack bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				var brokenPipe bool
 
+				if ne, ok := err.(*net.OpError); ok {
+					if se, ok := ne.Err.(*os.SyscallError); ok {
+						if strings.Contains(strings.ToLower(se.Error()), "broken pipe") || strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
+							brokenPipe = true
+						}
+					}
+				}
+
+				// 转储结构体
+				httpRequest, _ := httputil.DumpRequest(c.Request, true)
+
+				if brokenPipe {
+					logger.Error(
+						c.Request.URL.Path,
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+
+					c.Error(err.(error))
+					c.Abort()
+					return
+				}
+
+				if stack {
+					logger.Error(
+						"[Recovery from panic]",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+						zap.String("stack", string(debug.Stack())),
+					)
+				} else {
+					logger.Error(
+						"[Recovery from panic]",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+				}
+				c.AbortWithStatus(http.StatusInternalServerError)
+			}
+		}()
+		c.Next()
 	}
 }
