4e2821bf82731a327e0dea60ff0d6f09da84bc50
yangtao <1461481767@qq.com>
2022-09-22 02:35:12 +0800

    🚧 文件浏览

---
+++ pkg/explorer/executor.go
@@ -0,0 +1,138 @@
+package explorer
+
+import (
+	"io"
+	"io/fs"
+	"os"
+)
+
+var (
+	defaultsize int  = 10 << 10 // 默认buffersize 10MB
+	defaultmode uint = 0766     // 默认文件模式
+)
+
+// 文件操作执行器
+type Executor interface {
+
+	// 设置模式
+	SetMode(uint)
+
+	// 设置buffersize
+	SetBuffer(int)
+
+	// 获取模式
+	GetMode() uint
+
+	// 获取buffersize
+	GetBuffer() int
+
+	// 流上传
+	StreamUpload(string, io.Reader) error
+
+	// 流下载
+	StreamDownload(string) (io.Reader, error)
+
+	// 复制
+	Copy(string, string) error
+}
+
+// 默认执行器
+type NormalExecutor struct {
+	mode       uint // 文件模式
+	buffersize int  // buffer大小
+}
+
+var _ Executor = (*NormalExecutor)(nil)
+
+// todo: 新建执行器
+func NewNormalExecutor() *NormalExecutor {
+	exec := NormalExecutor{
+		mode:       defaultmode,
+		buffersize: defaultsize,
+	}
+
+	return &exec
+}
+
+// todo: 设置mode
+func (executor *NormalExecutor) SetMode(mode uint) {
+	executor.mode = mode
+}
+
+// todo: 获取mode
+func (executor *NormalExecutor) GetMode() uint {
+	return executor.mode
+}
+
+// todo: 设置buffer
+func (executor *NormalExecutor) SetBuffer(size int) {
+	executor.buffersize = size
+}
+
+// todo: 获取buffer
+func (executor *NormalExecutor) GetBuffer() int {
+	return executor.buffersize
+}
+
+// todo: 流上传
+func (executor *NormalExecutor) StreamUpload(path string, source io.Reader) error {
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, os.FileMode(executor.mode))
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	// 手动创建buffer
+	buf := make([]byte, executor.buffersize)
+
+	_, err = io.CopyBuffer(file, source, buf)
+
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}
+
+// todo: 流下载
+// 将文件句柄返回给gin进行下载
+//
+// 因此文件的句柄需要手动进行关闭
+func (executor *NormalExecutor) StreamDownload(path string) (io.Reader, error) {
+	file, err := os.OpenFile(path, os.O_RDONLY, fs.FileMode(executor.mode))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+// todo: 复制文件
+func (executor *NormalExecutor) Copy(source, dest string) error {
+	sourceFile, err := os.OpenFile(source, os.O_RDONLY, fs.FileMode(executor.mode))
+
+	if err != nil {
+		return err
+	}
+
+	defer sourceFile.Close()
+
+	destFile, err := os.OpenFile(dest, os.O_WRONLY|os.O_CREATE, fs.FileMode(executor.mode))
+
+	if err != nil {
+		return err
+	}
+
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, sourceFile)
+
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}
--- pkg/explorer/explorer.go
+++ pkg/explorer/explorer.go
@@ -1,12 +1,21 @@
 package explorer
 
-import "os"
+import (
+	"errors"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gabriel-vasile/mimetype"
+)
 
 // 文件抽象
 type File struct {
 	Name         string `json:"name"`         // 文件名
 	IsDirectory  bool   `json:"isDirectory"`  // 是否是文件夹
-	Size         int    `json:"size"`         // 大小
+	Size         int64  `json:"size"`         // 大小
 	Extension    string `json:"extension"`    // 扩展名
 	Mime         string `json:"mime"`         // 媒体类型
 	Path         string `json:"path"`         // 相对路径(相对于storagePath)
@@ -16,23 +25,155 @@ type File struct {
 // 文件管理器
 type Explorer struct {
 	rootDir    string   // 管理根目录
-	CurrentDir string   // 当前目录
+	currentDir string   // 当前目录
 	executor   Executor // 文件操作执行器
 }
 
 // todo: 返回管理器实例
 func NewDefaultExplorer(root string) *Explorer {
 
+	return &Explorer{
+		rootDir:    root,
+		currentDir: root,
+		executor:   NewNormalExecutor(),
+	}
 }
 
 // todo: 进入下一级目录
 func (exp *Explorer) EnterDir(dirname string) error {
+	newpath := filepath.Join(exp.currentDir, dirname)
 
+	if !dirExist(newpath) {
+		return errors.New("文件夹不存在")
+	}
+
+	exp.SetCurrentDir(newpath)
+
+	return nil
+}
+
+//todo: 创建目录
+func (exp *Explorer) CreateDir(dirname string) error {
+	newpath := filepath.Join(exp.currentDir, dirname)
+
+	if dirExist(newpath) {
+		return errors.New("目录已存在")
+	}
+
+	err := os.Mkdir(newpath, 0775)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// todo: 删除目录
+func (exp *Explorer) DeleteDir(dirname string) error {
+	newpath := filepath.Join(exp.currentDir, dirname)
+
+	if !dirExist(newpath) {
+		return errors.New("目录不存在")
+	}
+
+	err := os.RemoveAll(newpath)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// todo: 删除文件
+func (exp *Explorer) DeleteFile(filename string) error {
+	newpath := filepath.Join(exp.currentDir, filename)
+
+	err := os.Remove(newpath)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// todo: 返回当前目录
+func (exp *Explorer) GetCurrentDir() string {
+	return exp.currentDir
+}
+
+// todo: 设置当前目录
+func (exp *Explorer) SetCurrentDir(path string) {
+	exp.currentDir = path
+}
+
+// todo: 返回根目录
+func (exp *Explorer) GetRootDir() string {
+	return exp.rootDir
 }
 
 // todo: 列出当前目录内容
-func (exp *Explorer) ListContents() []*File {
+func (exp *Explorer) ListContents() ([]*File, error) {
+	// 返回 []direntity
+	files, err := os.ReadDir(exp.currentDir)
 
+	if err != nil {
+		return nil, err
+	}
+
+	if len(files) == 0 {
+		return []*File{}, nil
+	}
+
+	ret := []*File{}
+
+	for i := 0; i < len(files); i++ {
+		// 获取fileinfo
+		fileinfo, _ := files[i].Info()
+
+		file := exp.normalizeInfo(fileinfo)
+
+		if file != nil {
+			ret = append(ret, file)
+		}
+	}
+
+	return ret, nil
+}
+
+// todo: 规范化数据
+func (exp *Explorer) normalizeInfo(info fs.FileInfo) *File {
+
+	name := info.Name()
+
+	// 过滤
+	if name == "." || name == ".." {
+		return nil
+	}
+
+	afterSplit := strings.Split(name, ".")
+
+	// 获取文件扩展名
+	extension := afterSplit[len(afterSplit)-1]
+
+	filepath := filepath.Join(exp.currentDir, name)
+
+	// 获取mimetype
+	mime, _ := mimetype.DetectFile(filepath)
+
+	file := File{
+		Name:         name,
+		IsDirectory:  info.IsDir(),
+		Size:         info.Size(),
+		Path:         exp.currentDir,
+		Extension:    extension,
+		Mime:         mime.String(),
+		LastModified: info.ModTime().Unix(),
+	}
+
+	return &file
 }
 
 // todo: 返回执行器
@@ -43,6 +184,29 @@ func (exp *Explorer) GetExecutor() Executor {
 // todo: 配置执行器
 func (exp *Explorer) SetExecutor(executor Executor) {
 	exp.executor = executor
+}
+
+// todo: 流上传
+func (exp *Explorer) StreamUpload(path string, source io.Reader) error {
+	return exp.GetExecutor().StreamUpload(path, source)
+}
+
+// todo: 流下载
+func (exp *Explorer) StreamDownload(path string) (io.Reader, error) {
+	return exp.GetExecutor().StreamDownload(path)
+}
+
+// todo: 获取文件信息
+func (exp *Explorer) GetFileInfo(path string) (*File, error) {
+	info, err := os.Stat(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	file := exp.normalizeInfo(info)
+
+	return file, nil
 }
 
 // todo: 判断目录是否存在
